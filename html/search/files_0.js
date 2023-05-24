bd37c8ab20bd60dd09fba675266d5a41af1f384e
sudo-gera <gera.tatarinov.999@gmail.com>
2023-05-24 10:39:41 +0300

    contest_1_task_I

--- html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['b_24_5f_24_2ecpp_0',['B$_$.cpp',['../_b_0b___0b_8cpp.html',1,'']]]
-];
--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ict_5f2023_5f233_0',['ICT_2023_233',['../md__r_e_a_d_m_e.html',1,'']]]
-];
